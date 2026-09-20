bddbf5e30041a5195338b851aaab2b56b3cc0a9b
Barre jean-louis <jlmagbar@gmail.com>
2026-09-21 00:07:44 +0200

    Update GAS_EXEC_URL to new endpoint

--- Formulaire.js
+++ Formulaire.js
@@ -2,7 +2,7 @@
 // ===========================================================================
 // 1. CONFIGURATION GLOBAL ET VARIABLES D'ÉTAT
 // ===========================================================================
-const GAS_EXEC_URL = "https://script.google.com/macros/s/AKfycbyNQBIldYSDs7c1iKGBtzfLUzNB1QIb--9OjByeVbwhQt9kjtjdhLBJY8hXZ7ZbW_Gt/exec";
+const GAS_EXEC_URL = "https://script.google.com/macros/s/AKfycbx0pEZp1g99vgQghRXt85j8WDBa6m5-FM9AN--S3YXXQC8JyOHN8HWgNLedHSP2itH8/exec";
 
 let nbActiviteS = 0;
 let nbActiviteL = 0;   
